3ddc483ece6d71d978375ba96f978f98436b7da8
natallo7 <102995207+natallo7@users.noreply.github.com>
2022-05-24 19:56:05 +0200

    Showing current temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
+function showCurrentTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+}
 
+let apiKey = "1dff453389cac27359664305dda50aa0";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Amsterdam&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(showCurrentTemperature);
