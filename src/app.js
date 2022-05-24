fd6b71467e7415cb0078617db63a810b23106666
natallo7 <102995207+natallo7@users.noreply.github.com>
2022-05-24 20:28:52 +0200

    Removed console log

--- src/app.js
+++ src/app.js
@@ -22,7 +22,6 @@ function formatDate(timestamp) {
 }
 
 function showCurrentTemperature(response) {
-  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
