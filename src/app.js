4a39c38fcba7bb36484db5d2bc10b611dde6cf4a
natallo7 <102995207+natallo7@users.noreply.github.com>
2022-05-31 11:29:52 +0200

    Fixed issue with http urls

--- src/app.js
+++ src/app.js
@@ -42,7 +42,7 @@ function displayForecast(response) {
         `
     <div class="col-2">
       <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
-        <img src="http://openweathermap.org/img/wn/${
+        <img src="https://openweathermap.org/img/wn/${
           forecastDay.weather[0].icon
         }@2x.png"
         alt=""
@@ -89,7 +89,7 @@ function showCurrentTemperature(response) {
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
   iconElement.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 
